14a8a1bc25b0d3d1cd60a1ecb9faef249144766c
Cristian David Ramírez de la Rosa <crisronald3000@outlook.com>
2024-09-27 15:23:46 -0600

    Hacer un mock completo de un Custom Hook.

--- tests/components/GifGrid.test.jsx
+++ tests/components/GifGrid.test.jsx
@@ -1,11 +1,19 @@
-import { render, screen } from "@testing-library/react";
-import { GifGrid } from "../../src/components/GifGrid";
+import { render, screen } from '@testing-library/react';
+import { GifGrid } from '../../src/components/GifGrid';
+import { useFetchGifs } from '../../src/hooks/useFetchGifs';
+
+jest.mock('../../src/hooks/useFetchGifs');
 
 describe('Pruebas en <GifGrid />', () => {
 
     const category = 'Fortnite';
 
     test('debe de mostrar el loading inicialmente', () => {
+
+        useFetchGifs.mockReturnValue({
+            images: [],
+            isLoading: true,
+        });
 
         render(<GifGrid category={category} />);
         expect(screen.getByText('Cargando...'));
@@ -14,6 +22,27 @@ describe('Pruebas en <GifGrid />', () => {
     });
 
     test('debe de mostrar items cuando se cargan las imágenes useFetchGifs', () => {
+
+        const gifs = [
+            {
+                id: 'ABC',
+                title: 'Fortnite',
+                url: 'https://localhost/fortnite.jpg',
+            },
+            {
+                id: '123',
+                title: 'Goku',
+                url: 'https://localhost/goku.jpg',
+            },
+        ];
+        
+        useFetchGifs.mockReturnValue({
+            images: gifs,
+            isLoading: false,
+        });
+
+        render(<GifGrid category={category} />);
+        expect(screen.getAllByRole('img').length).toBe(2);
         
     });
 
