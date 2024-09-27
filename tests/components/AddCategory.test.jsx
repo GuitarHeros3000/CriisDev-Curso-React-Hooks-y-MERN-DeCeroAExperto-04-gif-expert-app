4f1a5b99d789d93843b5c42353007bdb36b45141
Cristian David Ramírez de la Rosa <crisronald3000@outlook.com>
2024-09-27 15:05:40 -0600

    Jest Functions.

--- tests/components/AddCategory.test.jsx
+++ tests/components/AddCategory.test.jsx
@@ -16,9 +16,9 @@ describe('Pruebas en <AddCategory />', () => {
 
     test('debe de llamar onNewCategory si el input tiene un valor', () => {
         const inputValue = 'Fortnite';
-        // TODO: ????
+        const onNewCategory = jest.fn();
 
-        render(<AddCategory onNewCategory={() => { }} />);
+        render(<AddCategory onNewCategory={onNewCategory} />);
 
         const input = screen.getByRole('textbox');
         const form = screen.getByRole('form');
@@ -27,7 +27,20 @@ describe('Pruebas en <AddCategory />', () => {
         fireEvent.submit(form);
         // screen.debug();
         expect(input.value).toBe('');
+        expect(onNewCategory).toHaveBeenCalled();
+        expect(onNewCategory).toHaveBeenCalledTimes(1);
+        expect(onNewCategory).toHaveBeenCalledWith(inputValue);
+    });
 
+    test('no debe de llamar onNewCategory si el input está vacío', () => {
+        const onNewCategory = jest.fn();
+        render(<AddCategory onNewCategory={onNewCategory} />);
+
+        const form = screen.getByRole('form');
+        fireEvent.submit(form);
+
+        expect(onNewCategory).toHaveBeenCalledTimes(0);
+        expect(onNewCategory).not.toHaveBeenCalled();
     });
 
 });
